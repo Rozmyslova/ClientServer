c934edd3eb83f3c55b1d8b73d7d9c817ae184a97
Dasha <Dasha@DESKTOP-4UQ3008>
2024-04-11 08:45:51 +0300

    починила для нового дз

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,9 +1,14 @@
-﻿static class Program
+﻿
+static class Program
 { 
-    
     static int Main()
     {
         ServerListener();
         return 0;
+    }
+
+    private static void ServerListener()
+    {
+        throw new NotImplementedException();
     }
 }
